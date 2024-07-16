c83203a04ce90e1a1541fff9b775fd8330bb9b51
KMK_HM <kyun9151@gmail.com>
2024-07-16 16:57:22 +0900

    feat: flyway 적용

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,37 +1,40 @@
-//package com.polarbookshop.catalogservice;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//import com.polarbookshop.catalogservice.domain.Book;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.test.web.reactive.server.WebTestClient;
-//
-//@SpringBootTest(
-//        webEnvironment = WebEnvironment.RANDOM_PORT
-//)
-//class CatalogServiceApplicationTests {
-//
-//    @Autowired
-//    private WebTestClient webTestClient;
-//
-//    @Test
-//    void whenPostRequestThenBookCreated() {
-//        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
-//
-//        webTestClient
-//                .post()
-//                .uri("/books")
-//                .bodyValue(expectedBook)
-//                .exchange() // 전송
-//                .expectStatus().isCreated()
-//                .expectBody(Book.class).value(actualBook -> {
-//                    assertThat(actualBook).isNotNull();
-//                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
-//                });
-//    }
-//
-//}
+package com.polarbookshop.catalogservice;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(
+        webEnvironment = WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles("integration")
+class CatalogServiceApplicationTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book(null, "1231231231", "Title", "Author", 9.90,
+                null, null, 0);
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange() // 전송
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+    }
+
+}
